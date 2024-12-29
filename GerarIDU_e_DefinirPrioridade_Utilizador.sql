321f0bd72b248e8533fd373e71aff40c5647141b
Brunocor26 <152163216+Brunocor26@users.noreply.github.com>
2024-12-29 18:01:37 +0000

    Update GerarIDU_e_DefinirPrioridade_Utilizador.sql

--- GerarIDU_e_DefinirPrioridade_Utilizador.sql
+++ GerarIDU_e_DefinirPrioridade_Utilizador.sql
@@ -1,46 +1,46 @@
 CREATE TRIGGER GerarIDU_e_DefinirPrioridade
 ON Utilizador
-INSTEAD OF INSERT
+INSTEAD OF INSERT --Utilizador insere tipo_utilizador, nome, telefone, email
 AS
 BEGIN
     DECLARE @tipo_utilizador CHAR(2);
     DECLARE @numero INT;
-    DECLARE @IDU VARCHAR(10);  -- Tamanho 10 para IDU (2 para tipo_utilizador + 1 para _ + 7 para o número sequencial)
+    DECLARE @IDU VARCHAR(10);  -- Tamanho 10 para IDU (2 para tipo_utilizador + 1 para _ + 7 para o nÃºmero sequencial)
     DECLARE @classe_prioridade VARCHAR(50);
     DECLARE @prioridade_corrente VARCHAR(50);
     
-    -- Obter o tipo de utilizador da inserção
+    -- obter o tipo de utilizador da inserÃ§ao
     SELECT @tipo_utilizador = tipo_utilizador FROM inserted;
     
-    -- Gerar o IDU no formato XX_0000000
-    -- Obter o próximo número sequencial para o tipo de utilizador
+    -- gerar IDU do tipo XX_0000000
+    -- obter o nÃºmero sequencial para o tipo de utilizador
     SELECT @numero = ISNULL(MAX(CAST(SUBSTRING(IDU, 4, 7) AS INT)), 0) + 1
     FROM Utilizador
     WHERE tipo_utilizador = @tipo_utilizador;
 
-    -- Gerar o IDU no formato XX_0000000 (7 dígitos para o número sequencial)
-    SET @IDU = CONCAT(@tipo_utilizador, '_', FORMAT(@numero, '0000000'));  -- 7 dígitos para o número sequencial
+    -- Gerar o IDU no formato XX_0000000 (7 dÃ­gitos para o nÃºmero sequencial)
+    SET @IDU = CONCAT(@tipo_utilizador, '_', FORMAT(@numero, '0000000'));  -- 7 dÃ­gitos para o nÃºmero sequencial
 
     -- Definir a classe de prioridade com base no tipo de utilizador
     IF @tipo_utilizador = 'PR'  -- Professores
     BEGIN
-        SET @classe_prioridade = 'Acima da Média';
+        SET @classe_prioridade = 'Acima da MÃ©dia';
     END
     ELSE IF @tipo_utilizador = 'PD'  -- Presidente do Departamento
     BEGIN
-        SET @classe_prioridade = 'Máxima';
+        SET @classe_prioridade = 'MÃ¡xima';
     END
     ELSE  -- Todos os outros tipos de utilizadores
     BEGIN
-        SET @classe_prioridade = 'Média';
+        SET @classe_prioridade = 'MÃ©dia';
     END
     
-    -- A prioridade corrente é inicialmente igual à classe de prioridade
+    -- a prioridade corrente no inicio Ã© igual Ã  classe de prioridade
     SET @prioridade_corrente = @classe_prioridade;
     
     -- Inserir o novo utilizador com o IDU gerado, classe de prioridade e prioridade corrente definidas
-    INSERT INTO Utilizador (IDU, tipo_utilizador, telefone, email, prioridade_corrente, classe_prioridade, nome)
-    SELECT @IDU, tipo_utilizador, telefone, email, @prioridade_corrente, @classe_prioridade, nome
+    INSERT INTO Utilizador (IDU, tipo_utilizador, nome, telefone, email, prioridade_corrente, classe_prioridade)
+    SELECT @IDU, tipo_utilizador, nome, telefone, email, @prioridade_corrente, @classe_prioridade
     FROM inserted;
 END;
 GO
